8c78dc675ee1eb9d70406408b270a258fc30a398
Jason Taylor <jetaylor@gmail.com>
2012-01-23 22:24:13 -0600

    Added pywol-listen and pywol.

---
+++ pywol-listen.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import socket
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind to any IP, port 9
+udp_socket.bind(('', 9))
+
+print 'PyWoL: Wake-On-LAN Listener\n=============================='
+print 'Listening for WoL packets on port 9.\n\n'
+
+while True:
+    data, _ = udp_socket.recvfrom(1024)
+
+    # Setup variable to hold our string.
+    payload = []
+    
+    for char in data:
+        raw_bit = hex(ord(char))
+        bit = raw_bit[2:]
+        if bit == '0':
+            bit = '00'
+        payload.append(bit)
+        
+    # Check if packet is actually a WoL packet, and starts with 6 0xFF
+    if payload[:6] == ['ff', 'ff', 'ff', 'ff', 'ff', 'ff']:
+        valid_wol = True
+    else:
+        valid_wol = False
+
+    if valid_wol:
+        # Remove 6 0xFF's
+        del payload[:6]
+        
+        # Compare two iterations of the MAC to make sure it's valid.
+        mac_address = payload[:6]
+        second_mac = payload[6:12]
+        
+        if mac_address == second_mac:
+            mac_address = payload[:6]
+            print 'WoL for MAC: ' + ":".join(mac_address)
---
+++ pywol.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# Notes:
+# I Need mac address, ip to send to.
+
+import sys
+import socket
+from binascii import unhexlify
+
+def build_packet(mac_string):
+	# mac_string will be a string of mac address
+	# Remove possible :'s or -'s
+	mac_hex = []
+	for char in mac_string:
+		if char != '-' and char != ':':
+			mac_hex.append(char)
+	
+	mac = "".join(mac_hex)
+	bytes = bytearray(102)
+	
+	position = 0
+	while position <= 5:
+		bytes[position] = b'\xff'
+		position += 1
+		
+	while position <= 101:
+		for byte in unhexlify(mac):
+			bytes[position] = byte
+			position += 1
+	
+	return bytes
+	
+	
+wol_packet = build_packet(sys.argv[1])
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(wol_packet, (sys.argv[2], 9))
